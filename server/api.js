16ef42bbc1cc8bcf0b53abcfaeba452c4721d1ed
Luiz Almeida <luizorst@hotmail.com>
2020-06-21 09:43:15 -0300

    Begin implementation of /meetings endpoint

--- server/api.js
+++ server/api.js
@@ -2,10 +2,13 @@ const express = require('express');
 const apiRouter = express.Router();
 
 
-const minionsRouter = require('./minions/minions');
+const minionsRouter = require('./minions');
 apiRouter.use('/minions', minionsRouter);
 
-const ideasRouter = require('./ideas/ideas')
+const ideasRouter = require('./ideas')
 apiRouter.use('/ideas', ideasRouter);
+
+const meetingsRouter = require('./meetings');
+apiRouter.use('/meetings', meetingsRouter);
 
 module.exports = apiRouter;
