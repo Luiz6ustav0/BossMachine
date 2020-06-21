db072513e2bc25e51600dadd772d227770e5e899
Luiz Almeida <luizorst@hotmail.com>
2020-06-21 10:19:26 -0300

    Finish implemening /meetings endpoint

--- server/meetings.js
+++ server/meetings.js
@@ -1,4 +1,27 @@
 const express = require('express');
+const db = require('./db');
+
 const meetingsRouter = express.Router();
+
+
+meetingsRouter.get('/', (req, res, next) => {
+    const meetingsArr = db.getAllFromDatabase('meetings');
+    if(meetingsArr){
+        res.send(meetingsArr);
+    } else {
+        res.sendStatus(204);
+    }
+});
+
+meetingsRouter.post('/', (req, res, next) => {
+    const newMeeting = db.addToDatabase('meetings', db.createMeeting());
+    res.status(201).send(newMeeting);
+});
+
+meetingsRouter.delete('/', (req, res, next) => {
+    db.deleteAllFromDatabase('meetings');
+    res.sendStatus(204);
+});
+
 
 module.exports = meetingsRouter;
